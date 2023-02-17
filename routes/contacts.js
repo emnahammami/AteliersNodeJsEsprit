15467080210c0f18317dad292971b8fd2c429e3c
emnahammami <emnahammami22@gmail.com>
2023-02-17 21:34:58 +0100

    2c

--- routes/contacts.js
+++ routes/contacts.js
@@ -23,6 +23,11 @@ var contact=require("../models/contact.js")
     );
     //res.json({message:'Hello'});
 });
+
+
+
+
+
   router.post('/', function(req, res, next) {
     new contact({
         FullName : req.body.FullName,
@@ -55,4 +60,50 @@ router.delete('/:id', function(req, res, next) {
   }}
 
 );
+
+router.get('/:id', function(req, res, next) {
+    try
+    {contact.findById(req.params.id, (err, cont)=> {
+      res.send(cont);
+    })
+       
+    // res.status(200).send({msg:"usser deleted",deleted})
+    
+   
+    }
+    catch (error) {
+        res.status(500).send("couldnt delete user")
+    
+    }}
+  
+  );
+
+
+router.put('/:id', function(req, res, next) {
+    contact.findByIdAndUpdate(
+        // the id of the item to find
+        req.params.id,
+        
+        // the change to be made. Mongoose will smartly combine your existing 
+        // document with this change, which allows for partial updates too
+        req.body,
+        
+        // an option that asks mongoose to return the updated version 
+        // of the document instead of the pre-updated one.
+        {new: true},
+        
+        // the callback function
+        (err, todo) => {
+        // Handle any possible database errors
+            if (err) return res.status(500).send(err);
+            return res.send(todo);
+        }
+    )}
+  
+  );
+
+
+
+
+
   module.exports = router;
